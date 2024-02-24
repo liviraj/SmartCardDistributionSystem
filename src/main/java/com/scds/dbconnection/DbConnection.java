7df8a9d1a2370c4de05c8fa3295b3707c48fd356
liviraj <arockiajayaraj805@gmail.com>
2024-02-24 13:57:26 +0530

    new

--- src/main/java/com/scds/dbconnection/DbConnection.java
+++ src/main/java/com/scds/dbconnection/DbConnection.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 public class DbConnection {
 	static Connection connection=null;
 	private static final String className="com.mysql.jdbc.Driver";
-	private static final String url="jdbc:mysql://localhost:3306/scds";
+	private static final String url="jdbc:mysql://127.0.0.1:3306/scds";
 	private static final String root="root";
 	private static String password="Sowbi@2024";
 	// private static final String password="Javaraj@007";
