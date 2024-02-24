24375ca7bb2c127b56832e1f7c216b3c49aff889
liviraj <arockiajayaraj805@gmail.com>
2024-02-24 12:52:24 +0530

    build

--- src/main/java/com/scds/dbconnection/DbConnection.java
+++ src/main/java/com/scds/dbconnection/DbConnection.java
@@ -11,7 +11,7 @@ public class DbConnection {
 	private static final String className="com.mysql.jdbc.Driver";
 	private static final String url="jdbc:mysql://localhost:3306/scds";
 	private static final String root="root";
-	private static String password="Historaj@007";
+	private static String password="Sowbi@2024";
 	// private static final String password="Javaraj@007";
 
 	public static Connection getConnection() throws ClassNotFoundException , SQLException{
